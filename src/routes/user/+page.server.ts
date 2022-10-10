c2df76caea701daad68c859f5a602d047ca6334e
Marius Berge Eide <mariusbe@gmail.com>
2022-10-10 04:38:06 +0200

    Connects to database and passes data to client nicely

--- src/routes/user/+page.server.ts
+++ src/routes/user/+page.server.ts
@@ -1,6 +1,89 @@
 import type { PageServerLoad, Actions } from './$types';
+import faunadb, { query as q } from "faunadb"
+import { FAUNADB_SECRET, FAUNADB_ENDPOINT } from '$env/static/private';
+
+type MyErr = string
+
+interface Result {
+    res?: any,
+    err?: string
+}
+
+type FaunaDoc = {
+    document: { ref: any, ts: number, data: object }
+}
+
+
+function dbClient(): faunadb.Client | undefined {
+    if (FAUNADB_SECRET !== undefined && FAUNADB_ENDPOINT !== undefined) {
+
+        const secret = FAUNADB_SECRET;
+        const endpoint = FAUNADB_ENDPOINT;
+
+        type Scheme = "http" | "https"
+        //var mg, domain, port: number, scheme: Scheme
+        var mg = endpoint.match(/^(https?):\/\/([^:]+)(:(\d+))?/)
+        if (!mg) {
+            mg = ["", "https", "db.fauna.com", "", "443"]
+        }
+
+        const scheme: Scheme = <Scheme>mg[1]
+        const domain: string = mg[2]
+        const port: number = Number(mg[4])
+        
+        return new faunadb.Client({
+            secret: secret,
+            domain: domain,
+            port: port,
+            scheme: scheme,
+        })
+
+    } else {
+        
+        return undefined
+    }
+}
+
+const client = dbClient();
+
+async function getUser() {
+    
+    let out: Result = {}
+
+    if (!client) {
+        out.err = "No DB connection";
+        return out
+    }
+
+    try { 
+        let res: FaunaDoc = await client.query(
+            q.Let(
+            {
+                doc: q.Get(q.Ref(q.Collection('users'), '345094312082538704')),
+            },
+            {
+                document: q.Var('doc')
+            }
+            )
+        )
+        out.res = res.document.data
+    }
+    catch (err) {
+        console.error(
+            'Error: [%s] %s: %s',
+            err.name,
+            err.message,
+            err.errors()[0].description,
+        );
+        out.err = `Error: [${err.name}] ${err.message}: ${err.errors()[0].description}`
+    }
+
+    //console.log("sending out:", out)
+    return out
+
+}
 
 export const load: PageServerLoad = ({ cookies }) => {
-    console.log(cookies)
-    return {hei: "test"}
+    console.log(cookies);
+    return getUser();
 }
--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
     import type { PageData, ActionData } from './$types';
+    import { readable } from 'svelte/store';
 
     export let data: PageData;
-    export let form: ActionData;
+    //export let form: ActionData;
 
     console.log(data);
 
+    $: res = readable(data.res);
+    $: err = readable(data.err)
+
 </script>
+{#if data.res}
+    <h1>Hei {$res.name}!</h1>
+    <p>E-post: {$res.email}</p>
+{/if}
+{#if data.err}
+    <p>Error: {$err}</p>
+{/if}
