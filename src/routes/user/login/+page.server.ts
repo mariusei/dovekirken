fbb07a263886d12426c1e5b49669329813543683
Marius Berge Eide <mariusbe@gmail.com>
2022-10-21 04:48:22 +0200

    Magic e-mail link with emoji sign in solution

--- src/routes/user/login/+page.server.ts
+++ src/routes/user/login/+page.server.ts
@@ -1,50 +1,117 @@
+import type { PageServerLoad, Actions } from "./$types";
+import type { Result } from '$lib/types'
+
+import { generateToken, addTokenToDB, verifyToken, deleteEmailToken } from '$lib/auth';
+import { findUserInDb } from "$lib/auth";
+
+
+export const load: PageServerLoad = async ({ cookies, url }) => {
+
+    const out: Result = {}
+
+    // User follows login URL
+    const auth: String | null = url.searchParams.get("auth")
+
+    if (!auth) {
+        out.err = "Auth not defined"
+        return out
+    }
+
+    console.log("proceeding to verufy token")
+
+    // Verify whether user email token exists
+    const isValidToken = await verifyToken(String(auth))
+
+    if (isValidToken.err) {
+        out.err = isValidToken.err
+        return out
+    }
+
+    console.log("have email and emoji?", isValidToken)
+
+    const emoji = isValidToken.res.data.emoji
+    const email = isValidToken.res.data.email
+
+    // Return possible emojis for user to choose from
+    const emojis = ['😍','😋','😐','🤩','😊','😀','😅','😴','😛','🤯','😨','🤠','🥳']
+    // randomize
+    let subset = [...emojis].sort(() => 0.5 - Math.random())
+    // remove correct emoji to prevent duplicates
+    subset.splice(subset.indexOf(emoji), 1)
+    // return 3 emojis to choose from
+    subset = subset.slice(0,3)
+
+    // index that we want user to verify
+    const ix = Math.floor(Math.random()*3)
+    subset[ix] = emoji
+
+    out.res = {
+        token: String(auth),
+        emojis: subset
+    }
+
+    return out
+}
 
 export const actions: Actions = {
-    followLink: async ({ request, cookies }) => {
-        const data = await request.formData()
-        const emailToken = data.get('emailToken')
+    // Form actions
+    // checks if user chooses correct emoji
+    // and then stores the user data to the store
+
+    check: async ({ cookies, request }) => {
+        const form = await request.formData()
+
+        const emojiChosen = form.get('emoji')
+        const emailToken = form.get("token")
 
         if (!emailToken) return { err: "Token field cant be empty" }
+        const isValidToken = await verifyToken(String(emailToken))
 
-        const isValidToken = await verifyEmailToken(String(emailToken))
+        const correctEmoji = isValidToken.res.data.emoji
 
         if (isValidToken.err) return {err: isValidToken.err}
         if (!isValidToken.res) return {err: "invalid token"}
 
-        // Retrieve user e-mail
-        const email = isValidToken.res.data.email
-
         // Delete the token from the e-mail
-        
         const deletedToken = await deleteEmailToken(String(emailToken))
 
         if (deletedToken.err) return {err: deletedToken.err}
         if (!deletedToken.res) return {err: "token didn't exist"}
 
+        // verify user seleceted emoji
+        console.log(isValidToken.res, isValidToken.res.emoji, emojiChosen)
+        if (!(correctEmoji == emojiChosen)) return {err: "invalid emoji"}
+
+        // Retrieve user e-mail
+        const email = isValidToken.res.data.email
+
+        // Retriever user ID
+        const userInDb = await findUserInDb(email)
+
+        if (userInDb.err) return {err: userInDb.err}
+        if (!userInDb.res) return {err: "user does not exist?"}
+
+        const userRef = userInDb.res.ref
+
+
         // Create new browser token
-
         const newToken = generateToken()
-        const addedSessionToken = await addTokenToDB(
-            email,
-            newToken,
-            "userTokens"
-        )
+        const addedSessionToken = await addTokenToDB({
+            email: email,
+            token: newToken,
+            collection: "userTokens",
+            userReference: userRef
+        })
 
-        // Create a new session token
-        cookies.set("token", newToken)
-        cookies.set("email", email)
+        // Create a new session cookie
+        cookies.set("session", newToken)
 
         // return session token to store
 
         return {
             status: "Authenticated",
-            //user: {
-            //    email: email,
-            //    token: newToken
-            //}
         }
 
-
-        console.log("received email token for processing: ", emailToken)
     }
 }
+
---
+++ src/routes/user/login/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import type { PageData, ActionData } from './$types';
+
+    export let data: PageData;
+    export let form: ActionData;
+
+</script>
+
+{#if data.err}
+<p>Error - {data.err}</p>
+{/if}
+
+{#if data.res}
+<p>Choose emoji - {data.res.emojis}</p>
+{/if}
+
+{#if data.res}
+<form method="POST">
+    <input type="hidden" id="token" name="token" value="{data.res.token}" />
+    {#each data.res.emojis as emoji}
+    <label>
+        <input type="radio" name="emoji" id="{emoji}" value="{emoji}">
+        {emoji}
+    </label>
+    {/each}
+    <button data-key="enter" formaction="?/check">Sjekk</button>
+
+</form>
+{/if}
+
+{#if form}
+<p>form data</p>
+<p>{form.err}</p>
+{/if}
