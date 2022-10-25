d82e862247e13deee7615ffeb72fe47c35e2d4a5
Marius Berge Eide <mariusbe@gmail.com>
2022-10-25 23:55:24 +0200

    Moved banner to Banner.svelte

---
+++ src/lib/Banner.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let heading: string = ""
+</script>
+
+<div class="font-light text-black
+bg-dkext-6
+-mt-6
+lg:-mx-4
+mb-6
+rounded-md
+p-4 pb-6
+
+">
+    <p class="text-sm">
+        Du er her: 
+        <a class="underline" href="/">Døvekirken</a> >
+    </p>
+    <h1 class="text-5xl">
+        {heading}
+    </h1>
+</div>
